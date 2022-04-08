58ba1fef0fb76fd4df98038278d884a4949eba87
Jan Herber <janherber@gmx.de>
2022-04-08 17:49:25 +0200

    simple click cli

--- src/k8s_resource_bot.py
+++ src/k8s_resource_bot.py
@@ -1,41 +1,36 @@
-from re import A
 import click
 import pathlib
 from resource_analyzer import (
     compute_configured_resources,
 )
 
-# @click.command()
-# @click.option(
-#     "--type",
-#     prompt="Enter the type (plain|helm)",
-#     help="The type of kubernetes manifests which should be analysed (plain|helm).",
-# )
-# @click.option(
-#     "--cluster-uri",
-#     prompt="Enter the uri of the desired kubernetes cluster",
-#     help="The uri of the kubernetes cluster to check for resource quota.",
-# )
-# @click.option(
-#     "--namespace-name",
-#     prompt="Enter the namespace name",
-#     help="The namespace name to analyse.",
-# )
-# def cmd(type, cluster_uri, namespace_name):
-def cmd():
-    # logger.info(
-    #     "Started with type %s in cluster %s for namespace %s",
-    #     type,
-    #     cluster_uri,
-    #     namespace_name,
-    # )
-    analyzed_workload_object = compute_configured_resources(
-        file_path=pathlib.Path(
-            "/home/jaydee/git/k8s-resource-bot/dev/test-case/example.yaml"
-        )
-    )
-    print(analyzed_workload_object.total_resources)
+
+@click.group()
+def cli() -> None:
+    """
+    This analyzer computes the total consumption of kubernetes workload objects.
+    """
+
+
+@cli.command()
+@click.option(
+    "--type",
+    default="plain",
+    help="The type of file to analyse. Possible options are [plain|helm]. Default is plain.",
+    required=True,
+    type=str,
+)
+@click.option(
+    "--file-path", help="Path to the plain file or helm chart.", required=True, type=str
+)
+def analyze(type, file_path) -> None:
+    if type == "plain":
+        analyzed_workload_object = compute_configured_resources(file_path)
+        print(analyzed_workload_object.total_resources)
+    else:
+        print("Wrong type!!!!")
 
 
 if __name__ == "__main__":
-    cmd()
+    cli.add_command(analyze)
+    cli()
